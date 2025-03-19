d5c50ba7daf71ebdcdc2640a53e332ea934e36c0
Yego <eddieyego273@gmail.com>
2025-03-19 13:39:16 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// code your solution herefunction superbowlWin(record){
+const record = [
+  { year: "2015", result: "w" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(record) {
+  const result = record.find((entry) => entry.result === "W");
+
+  if (result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+}
